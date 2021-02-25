29593a444272dd8c0a4d5df77dce071258348523
Yash Patel <pyash1691999@gmail.com>
2021-02-25 09:52:11 +0530

    Done Creating-Servers

--- Creating-Servers/src/app/app.component.ts
+++ Creating-Servers/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Creating-Servers';
+  serverElements = [{type: 'server', name: 'Testserver', content: 'just a test'}];
+  
+  onServerAdded(serverData : {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    }); 
+  }
+
+  onBlueprintAdded(blueprintData : {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
---
+++ Creating-Servers/src/app/server-element/server-element.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit, OnChanges {
+
+  @Input('svrElement') element: {type: string, name: string, content: string};
+
+  constructor() { 
+    console.log('constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+    
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+}
