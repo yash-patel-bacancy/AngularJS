1d93f0ee178c56d32baf1a2bf0d4b066f7bba54b
Yash Patel <pyash1691999@gmail.com>
2021-02-25 10:06:31 +0530

    Done Creating-Reactive-Forms

---
+++ Creating-Reactive-Forms/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
